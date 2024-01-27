bc8d7340050a6ea1eef704b01e7c101fdf048e28
JC2048 <jc2048.noreply@gmail.com>
2024-01-28 04:22:05 +0900

    Arg parser to translate Chinese cmd options

---
+++ src/script/argumentParser.ts
@@ -0,0 +1,44 @@
+import type { ChatInputCommandInteraction, Interaction } from 'discord.js';
+import { ApplicationCommandOptionType } from 'discord.js';
+
+const argumentNameMap: Record<string, string> = {
+  周目: "week",
+  王: "boss",
+  種類: "category",
+  補償刀: "leftover",
+  備註: "note",
+  成員: "user"
+}
+
+export async function argumentParser(interaction: ChatInputCommandInteraction, options: any[], exec: (interaction: ChatInputCommandInteraction, args: Record<string, any>) => Promise<void>) {
+  const args = {}
+  for (const option of options) {
+    const name = argumentNameMap[option.name]
+    console.log(name)
+    switch (option.type) {
+      case ApplicationCommandOptionType.Integer:
+        args[name] = await interaction.options.getInteger(option.name)
+        break
+      case ApplicationCommandOptionType.Number:
+        args[name] =  await interaction.options.getNumber(option.name)
+        break
+      case ApplicationCommandOptionType.String:
+        args[name] =  await interaction.options.getString(option.name)
+        break
+      case ApplicationCommandOptionType.Boolean:
+        args[name] =  await interaction.options.getBoolean(option.name)
+        break
+      case ApplicationCommandOptionType.Mentionable:  // user
+        args[name] =  await interaction.options.getMentionable(option.name)
+        break
+      case ApplicationCommandOptionType.Channel:
+        args[name] =  await interaction.options.getChannel(option.name)
+        break
+
+      default:
+        args[name] =  null
+
+    }
+  }
+  await exec(interaction, args)
+}
