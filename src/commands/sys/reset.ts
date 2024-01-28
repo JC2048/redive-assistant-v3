d8826caedcf5f9b0eabf1108e6e6ed3a0c56e982
JC2048 <jc2048.noreply@gmail.com>
2024-01-28 11:43:42 +0900

    reset placeholder

--- src/commands/sys/reset.ts
+++ src/commands/sys/reset.ts
@@ -4,15 +4,19 @@ import { SlashCommandBuilder } from 'discord.js';
 Reset all records of the bot
 */
 
-export default {
-  data: new SlashCommandBuilder()
-    .setName('reset')
-    .setDescription('重置所有紀錄'),
 
+const data = new SlashCommandBuilder()
+  .setName('reset')
+  .setDescription('重置所有紀錄')
+
+export default {
+  data: data,
   execute: async (interaction) => {
+
     await interaction.reply({
       content: "reset",
       ephemeral: true
-    });
-  },
+    })
+
+  }
 }
