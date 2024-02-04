4cd31b523c427602066ea38b3466e76dba2d2511
JC2048 <jc2048.noreply@gmail.com>
2024-02-05 08:18:41 +0900

    Allow db/setting to use new format as init

--- src/database/setting.ts
+++ src/database/setting.ts
@@ -1,3 +1,4 @@
+import { Guild } from 'discord.js'
 import { db } from '../index'
 import { GuildSetting, DatabaseGuildSetting } from '../types/Database'
 
@@ -5,21 +6,19 @@ export default {
 
   init: async (guildId: string) => {
 
-    const defaultSetting: Omit<GuildSetting, "id"> = {
+    const defaultSetting: GuildSetting = {
 
       guildId: guildId,
 
-      dashboard: {
-        knifeTable: {
-          channelId: "0",
-          messageId: "0",
-          topMessage: null,
-        },
-        memberTable: {
-          channelId: "0",
-          messageId: "0",
-          topMessage: null,
-        }
+      knifeTable: {
+        channelId: "0",
+        messageId: "0",
+        topMessage: null,
+      },
+      memberTable: {
+        channelId: "0",
+        messageId: "0",
+        topMessage: null,
       },
 
       user: {
@@ -48,5 +47,19 @@ export default {
       )
       console.log(`Created new setting for guild ${guildId}`)
     }
+  },
+
+  update: async (guildId: string, data: Partial<GuildSetting>) => {
+    try {
+      const setting = await db.collection('guild_setting').getFirstListItem(`guildId = "${guildId}"`)
+      await db.collection('guild_setting').update(
+        setting.id, data
+      )
+    } catch (e) {
+      console.log("[ERROR] Error while updating guild setting for guild " + guildId)
+      console.log(e)
+    }
+
+
   }
 }
