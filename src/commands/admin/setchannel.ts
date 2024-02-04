39cf070c8a0d5313eced53815d18e6e0128b1bad
JC2048 <jc2048.noreply@gmail.com>
2024-02-05 08:19:24 +0900

    added /setchannel

--- src/commands/admin/setchannel.ts
+++ src/commands/admin/setchannel.ts
@@ -1,34 +1,66 @@
 import { SlashCommandBuilder } from 'discord.js';
+import { client } from '../../index';
+import { setting } from '../../database';
+import { argumentParser } from '../../script/argumentParser';
 
 /*
 Set channel for dashboards
 */
+const data = new SlashCommandBuilder()
+.setName('setchannel')
+.setDescription('設定報刀/成員列表頻道')
+.addStringOption(option =>
+  option
+    .setName('列表')
+    .setDescription('列表')
+    .setRequired(true)
+    .setChoices(
+      { name: '報刀列表', value: 'knifeTable' },
+      { name: '成員列表', value: 'memberTable' },
+    )
+)
+.addChannelOption(option =>
+  option
+    .setName('頻道')
+    .setDescription('頻道')
+    .setRequired(true)
+)
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('setchannel')
-    .setDescription('設定報刀/成員列表頻道')
-    .addStringOption(option =>
-      option
-        .setName('category')
-        .setDescription('刷新類別')
-        .setRequired(true)
-        .setChoices(
-          { name: '報刀列表', value: 'knife' },
-          { name: '成員列表', value: 'member' },
-        )
-    )
-    .addChannelOption(option =>
-      option
-        .setName('channel')
-        .setDescription('頻道')
-        .setRequired(true)
-    ),
-
+  data: data,
   execute: async (interaction) => {
-    await interaction.reply({
-      content: "setchannel",
-      ephemeral: true
-    });
+    await argumentParser(interaction, data.options, async (interaction, args) => {
+
+      console.log(JSON.stringify(args, null, 2))
+
+      const channel = args.channel
+      if (!channel) {
+        await interaction.reply({
+          content: "頻道不存在!",
+          ephemeral: true
+        });
+        return
+      }
+
+      // TODO run board message generator
+      const boardMessage = await channel.send({
+        content: "This is a test message."
+      })
+
+      // update
+      await setting.update(interaction.guildId, {
+        [args.list]: {
+          channelId: channel.id,
+          messageId: boardMessage.id,
+        }
+      })
+
+      await interaction.reply({
+        content: "已設定頻道!",
+        ephemeral: true
+      });
+    })
+
+
   },
 }
--- src/script/argumentParser.ts
+++ src/script/argumentParser.ts
@@ -7,7 +7,10 @@ const argumentNameMap: Record<string, string> = {
   種類: "category",
   補償刀: "leftover",
   備註: "note",
-  成員: "user"
+  成員: "user",
+
+  頻道: "channel",
+  列表: "list",
 }
 
 export async function argumentParser(interaction: ChatInputCommandInteraction, options: any[], exec: (interaction: ChatInputCommandInteraction, args: Record<string, any>) => Promise<void>) {
