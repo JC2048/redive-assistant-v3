55a5e5541520ee2caaaaca58918fb20c14ebf604
JC2048 <jc2048.noreply@gmail.com>
2024-02-05 08:18:53 +0900

    Added data/init

--- src/database/data.ts
+++ src/database/data.ts
@@ -1,7 +1,37 @@
 import { db } from '../index'
+import { GuildData } from '../types/Database'
+import config from '../config'
 
 export default {
   init: async(guildId: string) => {
     
+    const defaultData: GuildData = {
+
+      guildId: guildId,
+
+      progress: [0, 0, 0, 0, 0],
+      hp: config.hp[0] as [number, number, number, number, number],
+
+      fullKnifeCount: 90,
+      leftoverKnifeCount: 0
+
+    }
+
+    // check if guild_data exists
+    try {
+      const oldData = await db.collection('guild_data').getFirstListItem(`guildId = "${guildId}"`)
+      // exists, overwrite
+      await db.collection('guild_data').update(
+        oldData.id, defaultData
+      )
+      console.log(`Overwritten old Data for guild ${guildId}`)
+    } catch (e) {
+      // does not exist, create new
+      await db.collection('guild_data').create(
+        defaultData,
+      )
+      console.log(`Created new Data for guild ${guildId}`)
+    }
+
   }
 }
