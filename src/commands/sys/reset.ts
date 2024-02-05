37af692548cd8328623ef4482103f195ded32cc2
JC2048 <jc2048.noreply@gmail.com>
2024-02-05 22:15:36 +0900

    Added /reset and corresponding function in db/user

--- src/commands/sys/reset.ts
+++ src/commands/sys/reset.ts
@@ -1,4 +1,6 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, } from 'discord.js';
+import { data as dbData, setting, user } from '../../database';
+import { client } from '../../index';
 
 /*
 Reset all records of the bot
@@ -13,10 +15,62 @@ export default {
   data: data,
   execute: async (interaction) => {
 
-    await interaction.reply({
-      content: "reset",
-      ephemeral: true
+    const response = await interaction.reply({
+      content: "此指令將會將所有報刀記錄及成員資料初始化!\n請確認是否繼續?",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder().addComponents(
+          new ButtonBuilder().setCustomId(interaction.id + "reset_confirm").setLabel("確定").setStyle(ButtonStyle.Danger),
+          new ButtonBuilder().setCustomId(interaction.id + "reset_cancel").setLabel("取消").setStyle(ButtonStyle.Secondary),
+        )
+      ]
     })
+
+    const collectorFilter = i => i.user.id === interaction.user.id
+
+    try {
+
+      const buttonPressed = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 })
+      if (buttonPressed.customId == interaction.id + "reset_confirm") {
+
+        const settings = await setting.get(interaction.guildId!)
+
+        // check prerequisite exists
+        if (settings.memberTable.channelId === "0" || settings.memberTable.messageId === "0") {
+          await interaction.editReply({ content: `尚未設定成員列表\n請先用\`/setchannel\`進行設定`, components: [] });
+          return
+        }
+
+        if (settings.knifeTable.channelId === "0" || settings.knifeTable.messageId === "0") {
+          await interaction.editReply({ content: `尚未設定報刀列表\n請先用\`/setchannel\`進行設定`, components: [] });
+          return
+        }
+
+        // perform init
+        await dbData.init(interaction.guildId!)
+
+        // delete all existing data
+        await user.guildDeleteAll(interaction.guildId)
+
+        // init for all valid user
+        const guildMembers = interaction.guild.members.cache.filter(member => member.roles.cache.some(role => role.id == settings.user.roleId))
+        const guildMemberIds = guildMembers.map(member => member.id) as string[]
+
+        await user.guildInit(interaction.guildId!, guildMemberIds)
+
+        // TODO update member and knife table
+
+        await interaction.editReply({ content: '已重置所有紀錄!', components: [] });
+        return
+
+      } else {
+        await interaction.editReply({ content: '已取消', components: [] });
+      }
+
+
+    } catch (e) {
+      await interaction.editReply({ content: '指令已逾時', components: [] });
+    }
 
   }
 }
--- src/database/user.ts
+++ src/database/user.ts
@@ -1,5 +1,61 @@
+import { UserData } from '../types/Database'
 import { db } from '../index'
 
 export default {
+
+  create: async (guildId: string, userId: string) => {
+
+    const data: UserData = {
+
+      userId: userId,
+      guildId: guildId,
+      knifeCount: 3,
+      leftoverCount: 0
+
+    }
+
+    await db.collection('user').create(
+      data
+    )
+
+  },
+
+  guildDeleteAll: async (guildId: string) => {
+
+    const guildUserDataList = await db.collection('user').getFullList({
+      filter: `guildId = "${guildId}"`,
+    })
+
+    const userDeletePromises = []
+    for (const guildUserData of guildUserDataList) {
+      userDeletePromises.push(db.collection('user').delete(guildUserData.id))
+    }
+    await Promise.all(userDeletePromises)
+
+  },
+
+  guildInit: async (guildId: string, userIds: string[]) => {
+
+    const userCreatePromises = []
+    for (const userId of userIds) {
+
+      userCreatePromises.push(
+        db.collection('user').create({
+          userId: userId,
+          guildId: guildId,
+          knifeCount: 3,
+          leftoverCount: 0
+        })
+          .then(() =>
+            console.log(`Created user ${userId} in guild ${guildId}`)
+          )
+      )
+
+    }
+
+    await Promise.all(userCreatePromises)
+
+  }
+
 
 }
