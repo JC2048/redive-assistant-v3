dba384f54fc17152d60e592366238724634af603
JC2048 <jc2048.noreply@gmail.com>
2024-02-22 08:11:40 +0900

    Allow record embed maker to accept more option, format change

--- src/script/RecordProcessor.ts
+++ src/script/RecordProcessor.ts
@@ -1,5 +1,5 @@
-import { Embed, EmbedBuilder, GuildMember, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord.js";
-import { DatabaseRecordData, RecordData } from "../types/Database";
+import { Collection, Embed, EmbedBuilder, GuildMember, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from "discord.js";
+import { DatabaseRecordData, ExpandedDatabaseRecordData, RecordData } from "../types/Database";
 import { knifeCategoryTranslator } from "./util";
 
 export enum RecordColor {
@@ -17,21 +17,27 @@ type RecordProcessorOptionOverride = {
   name?: string
   description?: string
   isCompleted?: boolean
+  nextActivator?: boolean
   damage?: number
+  footer?: string
 }
 
-export function recordEmbedGenerator(record: DatabaseRecordData | RecordData, guildMember: GuildMember, overrides: RecordProcessorOptionOverride = {}): EmbedBuilder {
+export function recordEmbedGenerator(record: ExpandedDatabaseRecordData | DatabaseRecordData | RecordData, guildMember: GuildMember, overrides: RecordProcessorOptionOverride = {}): EmbedBuilder {
 
   const embed = new EmbedBuilder()
     .setAuthor({
-      name: `${overrides.name ?? guildMember.nickname ?? guildMember.user.globalName ?? guildMember.user.username} |`
-        + ` ${record.isLeftover ? "🔶" : "🔷"}${record.isCompleted ?? overrides.isCompleted ?? false ? "✅" : ""}`
-        + ` ${record.week + 1}周 ${record.boss}王`,
+      name: `${overrides.name ?? guildMember.nickname ?? guildMember.user.globalName ?? guildMember.user.username}`,
       iconURL: guildMember.displayAvatarURL() ?? guildMember.user.avatarURL()
     })
-    .setDescription(overrides.description ?? `${knifeCategoryTranslator(record.category)} ${overrides.damage ?? record.damage}萬`)
+    .setTitle(
+      ` ${record.isLeftover ? "🔶" : "🔷"}${overrides.isCompleted ?? record.isCompleted ?? false ? "✅" : ""}${overrides.nextActivator ?? record.nextActivator ?? false ? " ↪️" : ""}`
+      + ` ${record.week + 1}周 ${record.boss}王`
+    )
+    .setDescription(overrides.description ?? `${knifeCategoryTranslator(record.category)} ${overrides.damage ?? record.damage ?? 0 > 0 ? `${record.damage}萬` : ""}`)
     // .setTimestamp()
     .setColor(overrides.color ?? (record.isLeftover ? RecordColor.LEFTOVER : RecordColor.NORMAL))
+
+  if (!!overrides.footer) embed.setFooter({ text: overrides.footer })
 
   /*
 
@@ -45,14 +51,19 @@ export function recordEmbedGenerator(record: DatabaseRecordData | RecordData, gu
 
 }
 
+type RecordStringSelectMenuBuilderOptions = {
+  placeholder?: string
+  minSelect?: number
+  maxSelect?: number
+}
+export function recordStringSelectMenuBuilder(interactionId: string, records: DatabaseRecordData[] | ExpandedDatabaseRecordData[], options: RecordStringSelectMenuBuilderOptions): StringSelectMenuBuilder
+export function recordStringSelectMenuBuilder(interactionId: string, records: ExpandedDatabaseRecordData[], options: RecordStringSelectMenuBuilderOptions, showName: true, guildMembers: Collection<string, GuildMember>): StringSelectMenuBuilder
 export function recordStringSelectMenuBuilder(
   interactionId: string,
-  records: RecordData[] | DatabaseRecordData[],
-  options?: {
-    placeholder?: string,
-    minSelect?: number,
-    maxSelect?: number
-  }): StringSelectMenuBuilder {
+  records: ExpandedDatabaseRecordData[] | DatabaseRecordData[],
+  options?: RecordStringSelectMenuBuilderOptions,
+  showName?: boolean,
+  guildMembers?: Collection<string, GuildMember>): StringSelectMenuBuilder {
 
   return new StringSelectMenuBuilder()
     .setCustomId(interactionId + "record_select")
@@ -60,8 +71,13 @@ export function recordStringSelectMenuBuilder(
     .addOptions([
       ...records.map(record =>
         new StringSelectMenuOptionBuilder()
-          .setLabel(`${record.isLeftover ? "🔶" : "🔷"}${record.week + 1}周${record.boss}王`)
-          .setDescription(`${knifeCategoryTranslator(record.category)}`)
+          .setLabel(`${!!showName
+            ? `${(() => {
+              const m = guildMembers.get(record.expand.user.userId)
+              return m?.nickname ?? m?.user.globalName ?? m?.user.username ?? ""
+            })()} | `
+            : ""}${record.isLeftover ? "🔶" : "🔷"}${record.week + 1}周${record.boss}王`)
+          .setDescription(`${knifeCategoryTranslator(record.category)} ${record.damage > 0 ? `${record.damage}萬` : ""}`)
           .setValue(record.id ?? "")
       )
     ])
@@ -69,3 +85,4 @@ export function recordStringSelectMenuBuilder(
     .setMaxValues(options?.maxSelect ?? 1)
 
 }
+
